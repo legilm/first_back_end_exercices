6b05e39c44d217d20fa690c9abe218b8e75a27c5
Gilmar <ogilmar@protonmail.com>
2025-04-07 14:23:47 +0200

    corretions

--- task5.R
+++ task5.R
@@ -12,9 +12,9 @@
 
 safe_divide <- function (a, b, round = FALSE)
 {
-  if (b == 0) {
-    return("Cannot divde by zero")
-  }
+  tryCatch({
+    if (!is.numeric(a) || !is.numeric(b)) stop("Invalid input")
+    if (b == 0) stop("Cannot divde by zero")
   if (is.na(a)){
     return(NA)
   } 
@@ -22,6 +22,9 @@ safe_divide <- function (a, b, round = FALSE)
     return(round((a / b),2))
   }
   return(a / b)
+  }, error = function(e) {a
+    return(e$message)
+  })
 }
 
 safe_divide(5, 3, round = TRUE)
